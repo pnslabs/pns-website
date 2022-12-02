b967f66a2a2add79b295912ef4db2b6ab510a50e
meisterjustice <eziefulejustice@gmail.com>
2022-12-02 15:27:10 +0100

    chore: fix accordion

--- components/Faqs.tsx
+++ components/Faqs.tsx
@@ -6,24 +6,39 @@ const items = [
   {
     title: 'What is PNS?',
     desc: 'PNS is a Phone Number Service that enables people to link their phone numbers to their wallet addresses on-chain. It also resolves wallet addresses to phone numbers.',
+    active: true,
   },
   {
     title:
       'If my phone is stolen, does the rouge actor with access to my phone number have access to my wallets?',
     desc: 'PNS is a Phone Number Service that enables people to link their phone numbers to their wallet addresses on-chain. It also resolves wallet addresses to phone numbers.',
+    active: false,
   },
   {
     title: 'What happens when I change my phone number?',
     desc: 'PNS is a Phone Number Service that enables people to link their phone numbers to their wallet addresses on-chain. It also resolves wallet addresses to phone numbers.',
+    active: false,
   },
   {
     title: 'How is my phone number verified?',
     desc: 'PNS is a Phone Number Service that enables people to link their phone numbers to their wallet addresses on-chain. It also resolves wallet addresses to phone numbers.',
+    active: false,
   },
 ];
 
 const Faqs = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [data, setData] = useState(items);
+
+  const handleActive = (index: number) => {
+    let newItems = [...data];
+    newItems[index].active = !newItems[index].active;
+    newItems.forEach((item, idx) => {
+      if (idx !== index) {
+        item.active = false;
+      }
+    });
+    setData(newItems);
+  };
   return (
     <div id="faqs" className="faqs">
       <div className="container">
@@ -41,22 +56,19 @@ const Faqs = () => {
         {items.map((item, index) => (
           <div key={index} className="faqs__wrapper">
             <div className="faqs__left-wrapper">
-              {index === activeIndex ? <FaqsYellowIcon /> : <FaqsBlackIcon />}
+              {item.active ? <FaqsYellowIcon /> : <FaqsBlackIcon />}
             </div>
             <div
-              onClick={() => setActiveIndex(index)}
+              onClick={() => handleActive(index)}
               className={`faqs__content-right-wrapper border ${
                 index === items.length - 1 && 'noborder'
               }`}>
-              <div
-                className={`faqs__arrow ${index === activeIndex && 'active'}`}>
+              <div className={`faqs__arrow ${item.active && 'active'}`}>
                 {'>'}
               </div>
               <div className="faqs__content-wrapper">
                 <div className="faqs__content-title">{item.title}</div>
-                <AnimateHeight
-                  duration={500}
-                  height={index === activeIndex ? 'auto' : 0}>
+                <AnimateHeight duration={500} height={item.active ? 'auto' : 0}>
                   <p className="faqs__content-desc">{item.desc}</p>
                 </AnimateHeight>
               </div>
--- components/GetStarted.tsx
+++ components/GetStarted.tsx
@@ -11,21 +11,35 @@ const items = [
     title: 'Search',
     desc: 'When you launch app, search for your mobile number in the search field provided and click on search for the system to scan, wait a few seconds to get a response and move on to the next 👇🏻',
     icon: <FaqsYellowIcon />,
+    active: true,
   },
   {
     title: 'Confirm mobile number',
     desc: 'PNS is a Phone Number Service that enables people to link their phone numbers to their wallet addresses on-chain. It also resolves wallet addresses to phone numbers.',
     icon: <GetStartedOneIcon />,
+    active: false,
   },
   {
     title: 'Connect',
     desc: 'PNS is a Phone Number Service that enables people to link their phone numbers to their wallet addresses on-chain. It also resolves wallet addresses to phone numbers.',
     icon: <GetStartedTwoIcon />,
+    active: false,
   },
 ];
 
 const GetStarted = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [data, setData] = useState(items);
+
+  const handleActive = (index: number) => {
+    let newItems = [...data];
+    newItems[index].active = !newItems[index].active;
+    newItems.forEach((item, idx) => {
+      if (idx !== index) {
+        item.active = false;
+      }
+    });
+    setData(newItems);
+  };
   return (
     <div className="getstarted">
       <div className="container">
@@ -40,30 +54,25 @@ const GetStarted = () => {
             <h2 className="getstarted__title">Getting started</h2>
           </div>
         </div>
-        {items.map((item, index) => (
+        {data.map((item, index) => (
           <div key={index} className="getstarted__wrapper">
             <div className="getstarted__left-wrapper">{item.icon}</div>
             <div
-              onClick={() => setActiveIndex(index)}
+              onClick={() => handleActive(index)}
               className={`getstarted__content-right-wrapper border ${
                 index === items.length - 1 && 'noborder'
               }`}>
-              <div
-                className={`getstarted__arrow ${
-                  index === activeIndex && 'active'
-                }`}>
+              <div className={`getstarted__arrow ${item.active && 'active'}`}>
                 {'>'}
               </div>
               <div className="getstarted__content-wrapper">
                 <div
                   className={`getstarted__content-title ${
-                    index === activeIndex && 'active'
+                    item.active && 'active'
                   }`}>
                   {item.title}
                 </div>
-                <AnimateHeight
-                  duration={500}
-                  height={index === activeIndex ? 'auto' : 0}>
+                <AnimateHeight duration={500} height={item.active ? 'auto' : 0}>
                   <p className="getstarted__content-desc">{item.desc}</p>
                 </AnimateHeight>
               </div>
