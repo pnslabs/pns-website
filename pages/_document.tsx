27fab86d460983dba479f77524f7bedded59a163
meisterjustice <eziefulejustice@gmail.com>
2023-02-27 06:46:12 +0400

    fix: fix twitter preview image url

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
           name="description"
           content="Unlock the power of Web3 with your mobile phone."
         />
+        <meta name="robots" content="all" />
         <link rel="icon" type="image/x-icon" href="/favicon.svg" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
@@ -34,19 +35,33 @@ export default function Document() {
         {/* Search Keywords */}
         <meta name="keywords" content="PNS, Web3, mobile phone" />
 
+        {/** See also */}
+        <meta property="og:see_also" content="https://twitter.com/pnslabs" />
+        <meta
+          property="og:see_also"
+          content="https://www.linkedin.com/company/pnslabs"
+        />
+
         {/** Twitter share */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@pnslabs" />
         <meta name="twitter:title" content="PNS" />
+        <meta name="twitter:creator" content="@pnslabs" />
         <meta
           name="twitter:description"
           content="Unlock the power of Web3 with your mobile phone."
         />
-
         <meta
-          property="twitter:image:src"
+          name="twitter:image"
           content="https://res.cloudinary.com/djzeufu4j/image/upload/v1677464416/pns-banner_d2fauh.jpg"
         />
+        <meta
+          name="twitter:image:alt"
+          content="An image containing the PNS logo and a text saying; Unlock your crypto"
+        />
+        <meta name="twitter:domain" content="https://usepns.xyz" />
+        <meta name="twitter:image:width" content="1024" />
+        <meta name="twitter:image:height" content="512" />
 
         {/** Google Analytics */}
         <Script
